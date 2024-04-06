d24840e8d68681cedbff8381857003878985bd5d
ShubhamP528 <shubham528prajapati@gmail.coom>
2024-04-06 09:16:37 +0530

    nav bar more responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -59,7 +59,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-gray-800">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-2 lg:px-4 max-[1088px]:text-xs ">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <div className="flex-shrink-0">
@@ -67,11 +67,11 @@ const Navbar = () => {
                 <img
                   src={logo}
                   alt="Logo"
-                  className="h-10 md:h-12 lg:h-14 w-auto rounded-full" // Adjusted size to h-10 md:h-12 lg:h-14
+                  className="h-10 md:h-10 lg:h-12 xl:mr-4 w-auto rounded-full"
                 />
               </Link>
             </div>
-            <div className="hidden md:block">
+            <div className="hidden lg:block">
               <div className="ml-10 flex items-baseline space-x-4">
                 <Link
                   to="/"
@@ -100,11 +100,11 @@ const Navbar = () => {
               </div>
             </div>
           </div>
-          <div className="hidden md:flex items-center space-x-4">
+          <div className="hidden lg:flex items-center space-x-4">
             {TheatorUser ? (
               <>
                 <button
-                  className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600 transition duration-300"
+                  className="bg-blue-500 text-white px-3 py-2 rounded hover:bg-blue-600 transition duration-300"
                   onClick={handleLinkForm}
                 >
                   Play by link
@@ -116,7 +116,7 @@ const Navbar = () => {
                   Welcome, {TheatorUser.username}!
                 </span>
                 <span className="text-gray-300 font-medium">
-                  Room :-
+                  Room:{" "}
                   <button
                     onClick={getInRoom}
                     className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
@@ -126,8 +126,7 @@ const Navbar = () => {
                 </span>
                 <button
                   onClick={handleShowAlert}
-                  className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600 transition duration-300"
-                  // className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="bg-blue-500 text-white px-3 py-2 rounded hover:bg-blue-600 transition duration-300"
                 >
                   Join New Room
                 </button>
@@ -157,7 +156,7 @@ const Navbar = () => {
               </>
             )}
           </div>
-          <div className="-mr-2 flex md:hidden">
+          <div className="mr-2 flex lg:hidden">
             <button
               onClick={() => setIsOpen(!isOpen)}
               type="button"
@@ -195,7 +194,7 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-      <div className={`${isOpen ? "block" : "hidden"} md:hidden`}>
+      <div className={`${isOpen ? "block" : "hidden"} lg:hidden`}>
         <div className="px-2 pt-2 pb-3 sm:px-3">
           <Link
             to="/"
@@ -224,7 +223,7 @@ const Navbar = () => {
           {TheatorUser ? (
             <>
               <button
-                className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600 transition duration-300"
+                className="bg-blue-500 text-white px-3 py-2 rounded hover:bg-blue-600 transition duration-300 mx-1"
                 onClick={handleLinkForm}
               >
                 Play by link
@@ -233,8 +232,8 @@ const Navbar = () => {
               <span className="block text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium">
                 Welcome, {TheatorUser.username}!
               </span>
-              <span className="text-gray-300 font-medium">
-                Room :-
+              <span className="text-gray-300 font-medium px-3 py-2">
+                Room:
                 <button
                   onClick={getInRoom}
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
@@ -244,7 +243,7 @@ const Navbar = () => {
               </span>
               <button
                 onClick={handleShowAlert}
-                className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600 transition duration-300"
+                className="bg-blue-500 text-white px-3 py-2 rounded hover:bg-blue-600 transition duration-300"
               >
                 Join New Room
               </button>
