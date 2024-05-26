8406d3d7fec21c3dbf557fe747caaecba169e54a
ShubhamP528 <shubham528prajapati@gmail.coom>
2024-05-26 10:12:10 +0530

    proxy changed

--- src/components/Ytplayer.js
+++ src/components/Ytplayer.js
@@ -5,7 +5,8 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useAuthcontext } from "../Contexts/AuthContext";
 
 // const socket = io("https://moviesappbackend-1.onrender.com"); // Adjust this to your server's address
-const socket = io("https://movies-app-backend-two.vercel.app"); // Adjust this to your server's address
+const socket = io("https://moviesappbackend.onrender.com"); // Adjust this to your server's address
+// const socket = io("https://movies-app-backend-two.vercel.app"); // Adjust this to your server's address
 // const socket = io("http://localhost:8080"); // Adjust this to your server's address
 
 function Ytplayer() {
