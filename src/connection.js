e8d9f4a54cc035ab920f7f9af65e819a0327fd21
ShubhamP528 <shubham528prajapati@gmail.coom>
2024-08-25 21:37:37 +0530

    api changed

--- src/connection.js
+++ src/connection.js
@@ -2,7 +2,7 @@ import io from "socket.io-client";
 
 // Initialize the socket connection
 // const socket = io("https://moviesappbackend-1.onrender.com"); // Adjust this to your server's address
-const socket = io("https://moviesappbackend.onrender.com"); // Adjust this to your server's address
-// const socket = io("https://movies-app-backend-two.vercel.app"); // Adjust this to your server's address
+// const socket = io("https://moviesappbackend.onrender.com"); // Adjust this to your server's address
+const socket = io("https://movies-app-backend-two.vercel.app"); // Adjust this to your server's address
 // const socket = io("http://localhost:8080"); // Adjust this to your server's address
 export default socket;
