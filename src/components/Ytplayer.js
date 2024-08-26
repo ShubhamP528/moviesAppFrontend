222905729ed5823eec094652f5af130c548587c7
ShubhamP528 <shubham528prajapati@gmail.coom>
2024-08-26 22:48:27 +0530

    Revert "Merge pull request #1 from ShubhamP528/test"
    This reverts commit 18e596b4f5c22ad85db41f3f0cd5f145fc91fee7, reversing
    changes made to d474005ff6c7466b8e82e291902c5c492cdb0a93.

--- src/components/Ytplayer.js
+++ src/components/Ytplayer.js
@@ -7,9 +7,7 @@ import YouTube from "react-youtube";
 
 // const socket = io("https://moviesappbackend-1.onrender.com"); // Adjust this to your server's address
 // const socket = io("https://moviesappbackend.onrender.com"); // Adjust this to your server's address
-
 // const socket = io("https://movies-app-backend-two.vercel.app"); // Adjust this to your server's address
-
 // const socket = io("http://localhost:8080"); // Adjust this to your server's address
 
 function Ytplayer() {
